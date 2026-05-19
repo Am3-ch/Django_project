41a5b966875e0fc2d2f7fed8aff0682663a40912
Humphrey Chama <humphreychama84@gmail.com>
2026-05-19 17:35:52 +0200

    changed the landing page

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+
 # Create your views here.
+@login_required
 def home(request):
     context = {
         'posts': Post.objects.all()
