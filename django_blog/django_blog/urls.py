d1459cd5ba3fa23fe870f66c05c2461a600d3712
Humphrey Chama <humphreychama84@gmail.com>
2026-06-06 04:44:08 +0200

    fixed url error

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -26,7 +26,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/', user_views.profile, name='profile'),
-    path('', include('blog.urls')),
+    path('home/', include('blog.urls')),
+    path('', user_views.landing_page, name='landing-page'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
